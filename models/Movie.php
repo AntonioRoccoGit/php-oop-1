9d6714c60a1962687976b8a7c095460fb68a5503
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-05-16 13:41:31 +0200

    added Bonus 1

---
+++ models/Movie.php
@@ -0,0 +1,26 @@
+<?php
+class Movie
+{
+    public $originalTtitle;
+    public $originalLenguage;
+    public $title;
+    public $genre;
+    public $poster;
+    public $description;
+    public $vote;
+
+    function __construct($_originalTitle)
+    {
+        $this->title = $_originalTitle;
+    }
+
+    public function printData()
+    {
+        return "Il film si chiama: {$this->title}";
+    }
+    public function setGenre(...$_genreID)
+    // i can set multiple genre id
+    {
+        $this->genre = $_genreID;
+    }
+}
